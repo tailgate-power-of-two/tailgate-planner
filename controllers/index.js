25b6451baef545484d6b6570cb72757cf68a341b
Alpha Bravo <bautista.albert@gmail.com>
2022-09-22 21:34:37 -0500

    Update views and layouts, initial heroku main and userhome

--- controllers/index.js
+++ controllers/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const dashboardRoutes = require('./dashboardRoutes');
+const homeRoutes = require('./homeRoutes');
 
 router.use('/api', apiRoutes);
 router.use('/dashboard', dashboardRoutes);
+router.use('/', homeRoutes);
 
 router.use((req, res) => {
   res.status(404).end();
--- utils/auth.js
+++ utils/auth.js
@@ -1,8 +1,10 @@
 const withAuth = (req, res, next) => {
-  // If the user is not logged in, redirect the request to the login route
-  if (!req.session.logged_in) {
+  // If the user is not logged in, redirect the user to the login page
+  if (!req.session.loggedIn) {
     res.redirect('/login');
   } else {
+    // If the user is logged in, execute the route function that will allow them to view the page
+    // We call next() if the user is authenticated
     next();
   }
 };
